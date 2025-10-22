6f6ffe6cdfe12a29eb9999db116debbc48994e34
99amanda99 <amanda.machado.godoy@escola.pr.gov.br>
2025-10-22 08:53:04 -0300

    .......

---
+++ assets/app.js
@@ -0,0 +1,4 @@
+function criaCartao{categoria,perguta,resposta}{
+    let container = document. getElementByld{'container'}
+    let cartao = document.createElement{'article'}
+    cartao.className='cartao'
