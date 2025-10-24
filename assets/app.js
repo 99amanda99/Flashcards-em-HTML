2d72e3e29203999d86cc01e644b9c8c15d0affa7
99amanda99 <amanda.machado.godoy@escola.pr.gov.br>
2025-10-24 10:43:38 -0300

    .....

--- assets/app.js
+++ assets/app.js
@@ -2,3 +2,14 @@ function criaCartao{categoria,perguta,resposta}{
     let container = document. getElementByld{'container'}
     let cartao = document.createElement{'article'}
     cartao.className='cartao'
+}
+cartao.innrHTML=`
+<div clss="cartao_conteudo">
+<h3>${categoria}</h3>
+<div class="cartao_conteudo_pergunta">
+<p>${pergunta}</p>
+</div>
+<div class="cartao_conteudo_resposta">
+<P>${resposta}</p>
+</div>
+</div>
